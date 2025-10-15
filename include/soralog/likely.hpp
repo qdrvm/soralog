81e023eb63c2a891dcd120064347cc06c3b3b0ef
turuslan <turuslan.devbox@gmail.com>
2025-10-15 16:09:48 +0500

    likely

--- include/soralog/likely.hpp
+++ include/soralog/likely.hpp
@@ -7,21 +7,29 @@
 
 #pragma once
 
-#if not defined(LIKELY_IF)
+#ifndef LIKELY_IF
 #if __cplusplus >= 202002L
-#define LIKELY_IF(x) [[likely]] if (x)
-#elif defined(__has_builtin) and __has_builtin(__builtin_expect)
-#define LIKELY_IF(x) if (__builtin_expect((x), 1))
+#define LIKELY_IF(x) if (x) [[likely]]
+#elif defined(__has_builtin)
+#if __has_builtin(__builtin_expect)
+#define LIKELY_IF(x) if (__builtin_expect(!!(x), 1))
+#else
+#define LIKELY_IF(x) if (x)
+#endif
 #else
 #define LIKELY_IF(x) if (x)
 #endif
 #endif
 
-#if not defined(UNLIKELY_IF)
+#ifndef UNLIKELY_IF
 #if __cplusplus >= 202002L
-#define UNLIKELY_IF(x) [[unlikely]] if (x)
-#elif defined(__has_builtin) and __has_builtin(__builtin_expect)
-#define UNLIKELY_IF(x) if (__builtin_expect((x), 0))
+#define UNLIKELY_IF(x) if (x) [[unlikely]]
+#elif defined(__has_builtin)
+#if __has_builtin(__builtin_expect)
+#define UNLIKELY_IF(x) if (__builtin_expect(!!(x), 0))
+#else
+#define UNLIKELY_IF(x) if (x)
+#endif
 #else
 #define UNLIKELY_IF(x) if (x)
 #endif
